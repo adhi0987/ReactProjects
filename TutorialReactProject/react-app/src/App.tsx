fdcec46c8e92e164ff2db96699131a97590a514d
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-05-20 21:17:53 +0530

    completed tutorial

--- TutorialReactProject/react-app/src/App.tsx
+++ TutorialReactProject/react-app/src/App.tsx
@@ -1,15 +1,24 @@
 import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
+import { useState } from "react";
 function App() {
-  let items = ["New York", "Spain", "Italy", "Russia"];
+  let items = ["New York", "Spain", "Italy", "Russia", "India"];
   const handleSelect = (item: string) => {
     console.log(item);
   };
+  const [AlertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Alert>
-        hello <span>world</span>
-      </Alert>
+      {AlertVisible && (
+        <Alert onClose={()=>setAlertVisibility(false)}>
+          hello <span>world</span>
+        </Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+      <hr></hr>
       <ListGroup items={items} heading="Cities" onSelectItem={handleSelect} />
     </div>
   );
