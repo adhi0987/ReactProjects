d58fd2143ebc03a336fbc0faa54f450595b175c5
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-05-21 20:46:15 +0530

    added the app folder

---
+++ ReactNativeMobile/spotlight-app/app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "red",
+    fontSize: 50,
+  },
+});
