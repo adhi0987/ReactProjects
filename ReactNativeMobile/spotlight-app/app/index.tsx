9babd2a7786b8a796805430d01586f7982a599b3
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-05-21 21:17:24 +0530

    added new files

--- ReactNativeMobile/spotlight-app/app/index.tsx
+++ ReactNativeMobile/spotlight-app/app/index.tsx
@@ -1,21 +1,15 @@
-import { Text, View, StyleSheet } from "react-native";
-
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../styles/auth.styles";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text style={styles.title}>Hello</Text>
+      <Image
+        source={{
+          uri: "https://m.media-amazon.com/images/S/pv-target-images/3bb9e19d96fdb24dcafbd535fe7692c2666ab74ce85728dd082e9b4189509a4e.jpg",
+        }}
+        style={{ width: 200, height: 200, resizeMode: "cover" }}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    color: "red",
-    fontSize: 50,
-  },
-});
