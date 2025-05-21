38c908831f95582f96a5ffed32b20163beb97a98
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-05-21 22:44:30 +0530

    some mistake with node modules

---
+++ ReactNativeMobile/spotlight-app/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../../styles/auth.styles";
+import { Link } from "expo-router";
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Link href={"/notifications"}>Feed Screen in tabs</Link>
+    </View>
+  );
+}
