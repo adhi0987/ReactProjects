009cb6cb94f1d162d6a12ce8d728fc167f719e0f
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-05-22 12:43:43 +0530

    added database

--- ReactNativeMobile/spotlight-app/app/_layout.tsx
+++ ReactNativeMobile/spotlight-app/app/_layout.tsx
@@ -1,26 +1,17 @@
 import { Stack } from "expo-router";
-import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
-import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { SafeAreaView } from "react-native";
 import InitialLayout from "@/components/InitialLayout";
 import { styles } from "@/styles/auth.styles";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 export default function RootLayout() {
-  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-  if (!publishableKey) {
-    throw new Error(
-      "Missing Publishable Key. please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
-    );
-  }
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-      <ClerkLoaded>
+    <ClerkAndConvexProvider>
         <SafeAreaProvider>
-          <SafeAreaView style={styles.container}>
+          <SafeAreaView style={{ flex:1, backgroundColor:"black"}}>
             <InitialLayout />
           </SafeAreaView>
         </SafeAreaProvider>
-      </ClerkLoaded>
-    </ClerkProvider>
+    </ClerkAndConvexProvider>
   );
 }
---
+++ ReactNativeMobile/spotlight-app/convex/auth.config.ts
@@ -0,0 +1,9 @@
+
+export default {
+  providers: [
+    {
+      domain: "https://ample-cardinal-32.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
