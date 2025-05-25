6b9d3504070f76fecf15795d3f5086ac856bd1ba
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-05-25 21:07:59 +0530

    added the profile page and its functionalities documentation yet to do

--- ReactNativeMobile/spotlight-app/convex/posts.ts
+++ ReactNativeMobile/spotlight-app/convex/posts.ts
@@ -205,3 +205,21 @@ export const deletePost = mutation({
     });
   },
 });
+
+export const getPostByUser = query({
+  args: { userId: v.optional(v.id("users")) },
+  handler: async (ctx, args) => {
+    const user = args.userId
+      ? await ctx.db.get(args.userId)
+      : await getAuthenticatedUser(ctx);
+
+    if (!user) throw new Error("user not found");
+
+    const posts = await ctx.db
+      .query("posts")
+      .withIndex("by_user", (q) => q.eq("userId", user._id))
+      .collect();
+
+    return posts;
+  },
+});
